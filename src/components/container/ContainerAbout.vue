38cb65f7b9747dfbc8cb809e0c0cf011ff70f448
Jelica Stanojevic <jelicastanojevicc@gmail.com>
2019-09-08 21:37:18 +0200

    Added container menu

--- src/components/container/ContainerAbout.vue
+++ src/components/container/ContainerAbout.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container-about">
     <div class="level">
-      <image-box class="level-left" image="spolja.jpg"></image-box>
+      <image-box
+        class="level-left level--overflow-hidden"
+        image="spolja.jpg"
+      ></image-box>
       <card
-        class="level-right"
+        class="level-right level--overflow-not-hidden"
         title="AVLIJA BAR"
         subtitle="Vlasotince"
         text="Lociran u Dorćolskom delu Vlasotinca, u blizini reke Vlasine, Avlija bar je mesto koje će Vas oduševiti svojom energijom i izvrsnom uslugom."
@@ -31,6 +34,10 @@ export default {
   background-image: url("../../assets/dot-pattern.png") !important;
 }
 
+.level-right {
+  margin: 0 !important;
+}
+
 @media screen and (min-width: 1020px) {
   .level {
     margin-right: 90px;
@@ -42,6 +49,13 @@ export default {
 
   .container-about {
     padding: 100px 0px;
+  }
+
+  .level--overflow-hidden {
+    z-index: 9;
+  }
+  .level--overflow-not-hidden {
+    z-index: 10;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <carousel :images="urls"></carousel>
     <container-about></container-about>
+    <container-menu></container-menu>
   </div>
 </template>
 
 <script>
 import Carousel from "@/components/Carousel";
 import ContainerAbout from "@/components/container/ContainerAbout";
+import ContainerMenu from "@/components/container/ContainerMenu";
+
 export default {
   name: "home",
   components: {
     Carousel,
-    ContainerAbout
+    ContainerAbout,
+    ContainerMenu
   },
   data() {
     return {
