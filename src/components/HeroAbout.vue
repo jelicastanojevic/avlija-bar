2b8c297ce03b38af79b19c41f28ec467c2bee80a
Jelica Stanojevic <jelicastanojevicc@gmail.com>
2019-09-08 22:03:34 +0200

    Added hero and about page

---
+++ src/components/HeroAbout.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="hero is-small is-bold hero--background">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns has-text-grey-light">
+          <div class="column column-icon--size">
+            <div class="container">
+              <i class="fa fa-phone"></i>
+            </div>
+          </div>
+          <div class="column column-title--size">
+            <div class="container ">
+              <strong> <span>REZERVACIJE</span></strong>
+              <br />
+              <strong> +381 64 64 00 022 <br /> </strong>
+            </div>
+          </div>
+
+          <div class="column column-icon--size">
+            <i class="fa fa-location-arrow"></i>
+          </div>
+          <div class="column column-title--size">
+            <div class="container">
+              <strong> <span>LOKACIJA</span></strong>
+              <br />
+              <strong> 22. Divizije 104 <br /> </strong>
+              Vlasotince
+            </div>
+          </div>
+
+          <div class="column column-icon--size">
+            <i class="far fa-clock"></i>
+          </div>
+          <div class="column column-title--size">
+            <div class="container">
+              <strong> <span>Radno vreme</span></strong>
+              <br />
+              8-22
+              <br />
+              <strong> PET-SUB <br /> </strong>
+              8-2
+              <br />
+            </div>
+          </div>
+
+          <div class="column column-icon--size">
+            <i class="fa fa-desktop"></i>
+          </div>
+          <div class="column column-title--size">
+            <div class="container">
+              <strong> <span>WEB</span></strong>
+              <br />
+              <strong> www.avlijabar.com <br /> </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.hero--background {
+  background-color: #191919;
+  background-image: url("../assets/brilliant.png");
+  border-top: 1rem solid #000;
+  border-bottom: 1rem solid #000;
+}
+
+.column-icon--size {
+  color: #fff8d5;
+  font-size: 4rem;
+  margin: auto;
+}
+
+.column-title--size {
+  color: #fff8d5;
+  font-size: 1.08rem;
+  line-height: 1.8rem;
+  margin: auto;
+}
+
+strong {
+  color: #fff8d5;
+}
+
+.button.is-large {
+  display: none;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,37 @@
 <template>
-  <div class="about"></div>
+  <div class="about">
+     <div class="image-container">
+      <img src="../assets/unutra.jpg" style="width:100%; height: 758px;"></img>
+    </div>
+    <hero-about></hero-about>
+    <container-about></container-about>
+  </div>
 </template>
+
+<script>
+import HeroAbout from "@/components/HeroAbout";
+import ContainerAbout from "@/components/container/ContainerAbout";
+
+export default {
+  name: "home",
+  components: {
+    HeroAbout,
+    ContainerAbout
+  }
+};
+</script>
+
+<style lang="scss">
+.about {
+  height: auto;
+  background-color: #0c0c0c;
+  background-image: url("../assets/dot-pattern.png");
+}
+
+.image-container {
+  height: 300px;
+  overflow: hidden;
+}
+@media screen and (min-width: 1020px) {
+}
+</style>
